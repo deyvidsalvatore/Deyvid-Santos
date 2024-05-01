8dbefab1d1f66641b1e41f0cf15a5a359c30bd02
Deyvid Santos <deyvidsantosdatascience@gmail.com>
2024-04-30 21:23:13 -0300

    Updates

--- src/app/data/certifications.data.ts
+++ src/app/data/certifications.data.ts
@@ -2,5 +2,9 @@ import { Certification } from "../_models/Certification";
 
 export const certifications: Certification[] = [
     {id: 10, title: 'Java COMPLETO 2023 Programação Orientada a Objetos + Projetos', obtained: 'July/2023', link: 'https://www.udemy.com/certificate/UC-a02ad198-2656-4756-8cef-1174d2fb7971/'},
-    {id: 11, title: 'Formação DBA SQL Server 2019 completo T-SQL, design e admin', obtained: 'May/2022', link: 'https://www.udemy.com/certificate/UC-9d8164b0-e310-41e3-8491-248fe38859d9/'}
+    {id: 11, title: 'Formação DBA SQL Server 2019 completo T-SQL, design e admin', obtained: 'May/2022', link: 'https://www.udemy.com/certificate/UC-9d8164b0-e310-41e3-8491-248fe38859d9/'},
+    {id: 12, title: 'Formação Angular Developer', obtained: 'April/2024', link: 'https://www.dio.me/certificate/YFO7LLRI/share'},
+    {id: 13, title: 'Formação React Developer', obtained: 'Feb/2023', link: 'https://www.dio.me/certificate/E6B391BC/share'},
+    {id: 14, title: 'Formação Programação Reativa com Spring WebFlux', obtained: 'Apr/2024', link: 'https://www.dio.me/certificate/7ZRIVLHA/share'},
+    {id: 15, title: 'Formação Front-end T6-ONE', obtained: 'Apr/2024', link: 'https://cursos.alura.com.br/degree/certificate/ae504fce-6cca-4fcd-abb0-3733447ac102?lang'}
 ];
